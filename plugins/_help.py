8a7634a265133e42285a37b14c061af2a0356966
ErzoA <96314386+ErzoA@users.noreply.github.com>
2023-03-02 09:47:20 +0800

    Update _help.py

--- plugins/_help.py
+++ plugins/_help.py
@@ -46,13 +46,13 @@ async def _help(ult):
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @AboutWiki"
                 await ult.eor(output)
             elif HELP.get("Addons") and plug in HELP["Addons"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Addons"][plug]:
                     output += i
-                output += "\n© @TeamUltroid"
+                output += "\n© @AboutWiki"
                 await ult.eor(output)
             elif HELP.get("VCBot") and plug in HELP["VCBot"]:
                 output = f"**Plugin** - `{plug}`\n"
